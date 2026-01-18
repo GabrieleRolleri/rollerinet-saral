2a30bcb79bd143ad8a4f79961dec5e9d61db9670
Gabriele Rolleri <rolleri@mit.edu>
2026-01-18 18:03:40 -0500

    Added content

--- src/consts.ts
+++ src/consts.ts
@@ -8,24 +8,20 @@ export const SITE_DESCRIPTION =
 // If you deploy to a subdirectory, set the `WEBSITE_BASE` here.
 // e.g. '/blog' if you are deploying to example.com/blog
 // Leave it as an empty string if you are deploying to a root domain (e.g. example.com)
-export const SITE_BASE = '/saral-theme-astro'
+export const SITE_BASE = ''
 
 export const SocialLinks = [
 	{
 		name: 'GitHub',
-		url: 'https://github.com/yashjawale',
+		url: 'https://github.com/GabrieleRolleri',
 	},
 	{
 		name: 'LinkedIn',
-		url: 'https://www.linkedin.com/in/yashjawale/',
-	},
-	{
-		name: 'X / Twitter',
-		url: 'https://twitter.com/imyashjawale',
+		url: 'https://www.linkedin.com/in/gabriele-rolleri/',
 	},
 	{
 		name: 'Instagram',
-		url: 'https://instagram.com/imyashjawale',
+		url: 'https://www.instagram.com/gabrielerolleri/',
 	},
 ]
 
@@ -35,8 +31,8 @@ export const WebsiteLinks = [
 		url: '',
 	},
 	{
-		name: 'Blog',
-		url: 'blog',
+		name: 'Photography',
+		url: 'photos',
 	},
 	{
 		name: 'About',
